65066f13631d09f1aa3e50d91a315302c17e97be
Paul Schoen <hi@paulschoen.me>
2026-01-30 15:31:51 -0600

    Add fish, nvim, ghostty configs

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+export PATH="$HOME/.local/bin:$PATH"
---
+++ dot_config/nvim/lua/plugins/symlink_theme.lua
@@ -0,0 +1,1 @@
+/home/paul/.config/omarchy/current/theme/neovim.lua
